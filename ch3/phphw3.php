1068aeb09bf1cc5ea7d1ad5e74516874d8e64dbd
GN2293 <GN071818@gmail.com>
2021-03-12 16:33:28 +0800

    Chapter 3

---
+++ ch3/phphw3.php
@@ -0,0 +1,12 @@
+<?php
+//Circule rate
+define('PI',3.1415926);
+//Circule radius
+$radius=10;
+//Calculate Circule Area
+$area=$radius*$radius*PI;
+echo "Circule area is ".$area."\n";
+//Cacule Circule Length
+$length=$radius*2*PI;
+echo "Circule Length is ".$length."\n";
+?>
