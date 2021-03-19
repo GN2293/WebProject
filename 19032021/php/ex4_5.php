96adbdc137bdda72d5d32bfab70826d7f064ca6a
GN2293 <GN071818@gmail.com>
2021-03-19 16:17:30 +0800

    origin

---
+++ 19032021/php/ex4_5.php
@@ -0,0 +1,13 @@
+<?php
+  $lunch = "咖哩饭";
+  switch($lunch){
+    case "牛肉面":
+      echo "每碗100元";
+      break;
+    case "咖哩饭":
+      echo "每碗80元";
+      break;
+    default:
+      echo "吃泡面";
+  }
+?>
