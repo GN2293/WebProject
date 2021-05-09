fa8fb8e979a5528a2689c3488c5633c3d97c8949
GN2293 <GN071818@gmail.com>
2021-05-09 15:52:53 +0800

    Add

---
+++ PHP/Comment.php
@@ -0,0 +1,11 @@
+<?php
+//Comments Training
+    echo "你爱他";
+    //echo '真的吗?';
+    print "他不爱你";
+    /*
+    以下是猜的
+    print '他有外遇';
+    printf ("你有外遇");
+    */
+?>
---
+++ PHP/Printer.php
@@ -0,0 +1,7 @@
+<?php
+    echo "Hello";
+    echo "World";
+    echo "Hello02";
+    echo "World02";
+    printf("Hello World");
+?>
