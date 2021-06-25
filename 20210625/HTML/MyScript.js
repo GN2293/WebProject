01391ed4f561913fb2988269d0b731d1f07423f3
GN2293 <GN071818@gmail.com>
2021-06-25 11:59:56 +0800

    Add

---
+++ 20210625/HTML/MyScript.js
@@ -0,0 +1,6 @@
+function MyDemo(){
+    //const car = {type:"Camaro",model:"500",color:"Black"};
+    //car.color = "yellow";
+    //car.owner = "GN";
+    document.getElementById("demo").innerHTML ="Car owner is "+car.owner;
+}
