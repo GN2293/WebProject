f8ca5b2eb79bb7465b70bc84cd5572f11ad0139d
GN2293 <GN071818@gmail.com>
2021-06-25 12:07:13 +0800

    Add

--- 20210625/HTML/MyScript.js
+++ 20210625/HTML/MyScript.js
@@ -2,5 +2,5 @@ function MyDemo(){
     //const car = {type:"Camaro",model:"500",color:"Black"};
     //car.color = "yellow";
     //car.owner = "GN";
-    document.getElementById("demo").innerHTML ="Car owner is "+car.owner;
+    document.getElementById("demo").innerHTML =person.firstName + " is " + person.age + " years old.";
 }
